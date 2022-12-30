cc68fa3591d8406043397be5da730ede43472a99
Adhiana <adhianamastur48@gmail.com>
2022-12-30 22:57:48 +0700

    fix routing client and posts

--- client-react/src/CommentCreate.js
+++ client-react/src/CommentCreate.js
@@ -7,7 +7,7 @@ const CommentCreate = ({ postId }) => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axios.post(`http://microservice-1st-app.com/posts/${postId}/comments`, {
       content,
     });
 
--- client-react/src/PostCreate.js
+++ client-react/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", {
+    await axios.post("http://microservice-1st-app.com/posts/create", {
       title,
     });
 
